582b4dabc14f802ff59c23a87f4648e4f675ae8c
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-31 11:55:11 +0300

    Fixed some test titles

--- cypress/e2e/working-with-apis.cy.js
+++ cypress/e2e/working-with-apis.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-it('First test', () => { // intercept has to be created before the actual call is made by the browser
+it('API Mocking', () => { // intercept has to be created before the actual call is made by the browser
   cy.intercept('GET', '**/tags', { fixture: 'tags.json'})
   cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
   cy.logIn()
 })
 
-it.only('First test', () => {
+it.only('Modify API Response', () => {
   cy.intercept('GET', '**/articles*', req => {
     req.continue(res => {
       res.body.articles[0].favoritesCount = 9999999
