e9ac73ebcb0ceecd65be204cb3928047fb07522d
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-31 15:58:22 +0300

    Completed the Route Matcher and Waiting forBrowser API Calls lessons

--- cypress/e2e/working-with-apis.cy.js
+++ cypress/e2e/working-with-apis.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
 
 it('API Mocking', () => { // intercept has to be created before the actual call is made by the browser
-  cy.intercept('GET', '**/tags', { fixture: 'tags.json'})
+  // cy.intercept('GET', '**/tags', { fixture: 'tags.json'})
+  cy.intercept({ method: 'GET', pathname: 'tags' }, { fixture: 'tags.json' }) // cleaner alternative for the above method
   cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
   cy.logIn()
 })
 
 it.only('Modify API Response', () => {
-  cy.intercept('GET', '**/articles*', req => {
+  cy.intercept({ method: 'GET', pathname: 'articles' }, req => {
     req.continue(res => {
       res.body.articles[0].favoritesCount = 9999999
       res.send(res.body)
@@ -15,4 +16,17 @@ it.only('Modify API Response', () => {
   })
   cy.logIn()
   cy.get('app-favorite-button').first().should('contain.text', '9999999')
+})
+
+it('Waiting for APIs', () => {
+  cy.intercept({ method: 'GET', pathname: 'articles' }).as('articleApiCall')
+  cy.logIn()
+  cy.wait('@articleApiCall').then(apiArticleObject => {
+    // console.log(apiArticleObject)
+    expect(apiArticleObject.response.body.articles[0].title).to.contain('Bondar Academy')
+  })
+  // cy.get('app-article-list').should('contain.text', 'Bondar Academy')
+  cy.get('app-article-list').invoke('text').then(allArticleTexts => { // this assertion will fail because invoke will assert the text which comes first, which in this case is "Loading articles...", so you need to add a dynamic wait before
+    expect(allArticleTexts).to.contain('Bondar Academy')
+  })
 })
