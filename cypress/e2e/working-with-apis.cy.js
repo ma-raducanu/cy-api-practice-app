70db725ebabc720372bc2716e74195da09ceebd9
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-31 11:53:39 +0300

    Completed the Modify API Response lesson

--- cypress/e2e/working-with-apis.cy.js
+++ cypress/e2e/working-with-apis.cy.js
@@ -5,3 +5,14 @@ it('First test', () => { // intercept has to be created before the actual call i
   cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })
   cy.logIn()
 })
+
+it.only('First test', () => {
+  cy.intercept('GET', '**/articles*', req => {
+    req.continue(res => {
+      res.body.articles[0].favoritesCount = 9999999
+      res.send(res.body)
+    })
+  })
+  cy.logIn()
+  cy.get('app-favorite-button').first().should('contain.text', '9999999')
+})
