cb12401ba61f09b0703f37358d1dabbcef79f4b3
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-09-02 14:34:10 +0300

    Completed the Multiple Test Reports and Cypress with Docker Container lessons

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,14 @@ module.exports = defineConfig({
     password: 'Testing123!',
     apiUrl: 'https://conduit-api.bondaracademy.com/api'
   },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
   e2e: {
     baseUrl: 'https://conduit.bondaracademy.com/',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // $env:USERNAME='mircea.alexandru.vi.raducanu@gmail.com' ; $env:PASSWORD='Testing123!' ; npm run cy-run
       // config.env.username = process.env.USERNAME,
       // config.env.password = process.env.PASSWORD
